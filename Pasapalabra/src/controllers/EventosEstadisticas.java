2329d5bb5901a4df7a13a5dd5de33bd760747545
Asier Gutiérez <asierguti96@gmail.com>
2016-01-21 03:21:21 +0100

    Update EventosEstadisticas.java

--- Pasapalabra/src/controllers/EventosEstadisticas.java
+++ Pasapalabra/src/controllers/EventosEstadisticas.java
@@ -2,13 +2,16 @@ package controllers;
 
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.Random;
 import java.util.ResourceBundle;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Control;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
 import javafx.scene.shape.Circle;
 import javafx.scene.shape.Rectangle;
@@ -20,31 +23,22 @@ public class EventosEstadisticas extends ClaseExtensora implements Initializable
     private Text textoESPanel;
 
     @FXML
-    private Text textoPlus;
+    private Text txtPartidasJugadas;
 
     @FXML
     private Text textoMiPerfil;
 
     @FXML
-    private ImageView logopsp;
-
-    @FXML
-    private Text textoLogeadoComo;
-
-    @FXML
-    private Text textoCerrarSesion;
-
-    @FXML
     private Rectangle rectanguloPanel;
-
-    @FXML
-    private Rectangle rectanguloCerrarSesion;
 
     @FXML
     private Rectangle rectanguloAmigos;
 
     @FXML
-    private Text textoJugar;
+    private Text txtDerrotas;
+
+    @FXML
+    private Text txtEmpates;
 
     @FXML
     private Rectangle rectanguloMiPerfil;
@@ -56,6 +50,39 @@ public class EventosEstadisticas extends ClaseExtensora implements Initializable
     private Text textoAmigos;
 
     @FXML
+    private ImageView imagenAvatar;
+
+    @FXML
+    private Pane panel;
+
+    @FXML
+    private Circle circuloPlus;
+
+    @FXML
+    private Text txtPosicion;
+
+    @FXML
+    private Text textoPlus;
+
+    @FXML
+    private ImageView logopsp;
+
+    @FXML
+    private Text textoLogeadoComo;
+
+    @FXML
+    private Text textoCerrarSesion;
+
+    @FXML
+    private Text txtRatio;
+
+    @FXML
+    private Rectangle rectanguloCerrarSesion;
+
+    @FXML
+    private Text textoJugar;
+
+    @FXML
     private Circle circuloPanel;
 
     @FXML
@@ -65,16 +92,10 @@ public class EventosEstadisticas extends ClaseExtensora implements Initializable
     private Text textoEstadisticas;
 
     @FXML
-    private ImageView imagenAvatar;
-
-    @FXML
     private Rectangle rectanguloEstadisticas;
 
     @FXML
-    private Pane panel;
-
-    @FXML
-    private Circle circuloPlus;
+    private Text txtVictorias;
     
     public static ArrayList<String> Datos_Usuario_Estadisticas=new ArrayList<String>();;
     /*Posiciones:
@@ -85,55 +106,78 @@ public class EventosEstadisticas extends ClaseExtensora implements Initializable
      * 4: posición en el ranking
      */
     @FXML
-    void btnJugar(ActionEvent event) {
-
+    void btnJugar(MouseEvent event) {
+    	utilidades.deVentana.transicionVentana("Juego", event);
     }
 
     @FXML
-    void btnMiPerfil(ActionEvent event) {
+    void btnMiPerfil(MouseEvent event) {
+    	utilidades.deVentana.transicionVentana("Perfil", event);
+    }
+
+	@FXML
+	void entrado(MouseEvent event) {
+		utilidades.deVentana.efectoTransparenciaOnHover(event, this);
+	}
+
+	//Añade nivel de transparencia
+	@FXML
+	void salido(MouseEvent event) {
+		utilidades.deVentana.efectoTransparenciaOnHover(event, this);
+	}
+
+    @FXML
+    void btnEstadisticas(MouseEvent event) {
 
     }
 
-    @FXML
-    void entrado(ActionEvent event) {
 
+    @FXML
+    void btnAmigos(MouseEvent event) {
+    	utilidades.deVentana.transicionVentana("Amigos", event);
     }
 
     @FXML
-    void salido(ActionEvent event) {
-
+    void btnCerrarSesion(MouseEvent event) {
+    	utilidades.deVentana.cerrarSesion(event);
     }
 
     @FXML
-    void btnEstadisticas(ActionEvent event) {
-
-    }
-
-
-    @FXML
-    void btnAmigos(ActionEvent event) {
-
-    }
-
-    @FXML
-    void btnCerrarSesion(ActionEvent event) {
-
-    }
-
-    @FXML
-    void esPanel(ActionEvent event) {
+    void esPanel(MouseEvent event) {
 
     }
 
 
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
-		// TODO Auto-generated method stub
-		
-		for (String string : Datos_Usuario_Estadisticas) {
-			System.out.println(string);
+		if(EventosLogIn.iAvatar!=null){
+			imagenAvatar.setImage(EventosLogIn.iAvatar);
+		}else{
+			String imagen = "fPerfil";
+			Random rand = new Random();
+			int randomNum = rand.nextInt((1000 - 1) + 1) + 1;
+			if(randomNum == 666){
+				imagen = "fPerfilPirata";
+			}
+
+			Image i = new Image("images/"+ imagen +".png",imagenAvatar.getBoundsInLocal().getWidth(),imagenAvatar.getBoundsInLocal().getHeight(),false,true);
+			imagenAvatar.setImage(i);
 		}
+		Circle clip = new Circle((imagenAvatar.getX()+imagenAvatar.getBoundsInParent().getWidth())/2, (imagenAvatar.getY()+imagenAvatar.getBoundsInParent().getHeight())/2, imagenAvatar.getBoundsInLocal().getHeight()/2);
+		imagenAvatar.setClip(clip);
+		imagenAvatar.setSmooth(true); 
+		imagenAvatar.setCache(true); 
+		textoNombreDeUsuario.setText(utilidades.Conexion_cliente.Datos_Usuario.get(0));
+		
+		
+		
+		txtDerrotas.setText(Datos_Usuario_Estadisticas.get(1));
+		txtEmpates.setText(Datos_Usuario_Estadisticas.get(2));
+		txtVictorias.setText(Datos_Usuario_Estadisticas.get(0));
+		double d = (Integer.parseInt(Datos_Usuario_Estadisticas.get(0))/Integer.parseInt(Datos_Usuario_Estadisticas.get(1)));
+		txtRatio.setText(String.valueOf(d));
+		txtPosicion.setText(Datos_Usuario_Estadisticas.get(4));
+		txtPartidasJugadas.setText(Datos_Usuario_Estadisticas.get(3));
 	}
 
 }
-
