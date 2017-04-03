161ad99301a2042050ab862064eaa0b19d02a3dc
maro01507i <ivan.martin@opendeusto.es>
2017-04-03 16:35:57 +0200

    Added new utilities classes for connection purposes
    Three new classes have been added for gameplay reasons, also updated
    old	Enum
    
    
    
    
    Signed-off-by: maro01507i <ivan.martin@opendeusto.es>

---
+++ Pasapalabra/serv/model/QuestionType.java
@@ -0,0 +1,9 @@
+package model;
+
+/**Enum de posibles elecciones sobre tipos de preguntas.
+ * @author asierguti
+ *
+ */
+public enum QuestionType {
+	All,Geography,Art,History,Science,Entertaining,Sport
+}
