b6e3fe1dce67a31c0a0bedf05e395a8ca5929b32
maro01507i <ivan.martin@opendeusto.es>
2017-04-03 16:37:01 +0200

    Added new classes for session control purposes
    3 new token related classes have been added for session control
    
    
    Signed-off-by: maro01507i <ivan.martin@opendeusto.es>

---
+++ Pasapalabra/serv/service/auth/Token.java
@@ -0,0 +1,31 @@
+package service.auth;
+
+import java.io.Serializable;
+
+public class Token implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+	private String token;
+	
+	public Token(String token){
+		this.token = token;
+	}
+	
+	public String getToken(){
+		return this.token;
+	}
+	
+	public void setToken(String token){
+		this.token = token;	
+	}
+
+	public boolean equals(Token token) {
+		return this.token.equals(token.getToken());
+	}
+	
+	@Override
+	public int hashCode() {
+		return token.hashCode();
+	}
+	
+}
