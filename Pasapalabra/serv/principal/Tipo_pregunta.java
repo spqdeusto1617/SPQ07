0488b6e76715540777013245bc505d537eb860ac
maro01507i <ivan.martin@opendeusto.es>
2017-04-05 17:01:19 +0200

    Removed unused class
    One unused class has been removed
    
    
    Signed-off-by: maro01507i <ivan.martin@opendeusto.es>

--- Pasapalabra/serv/principal/Tipo_pregunta.java
+++
@@ -1,9 +0,0 @@
-package principal;
-
-/**Enum de posibles elecciones sobre tipos de preguntas.
- * @author asierguti
- *
- */
-public enum Tipo_pregunta {
-	Todos,Geografía,Arte,Historia,Ciencia,Entretenimiento,Deportes
-}
